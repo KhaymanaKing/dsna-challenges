c3cf45e24d9239ec01956360a750cb6ca493ddb8
Khayman King <khaymanak@gmail.com>
2022-08-15 09:39:29 -0700

    added to test

--- day-01/array.test.js
+++ day-01/array.test.js
@@ -11,4 +11,7 @@ test("array push", () => {
     const arr = ['a', 'b', 'c'];
     const newLength = push(arr, 'd');
     expect(arr).toEqual(['a', 'b', 'c', 'd'])
-})
+    expect(newLength).toBe(4);
+}); 
+
+
