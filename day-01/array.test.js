c673178bdc54b9afb6df14a168f3dd74bcc3cbf6
Khayman King <khaymanak@gmail.com>
2022-08-15 09:47:51 -0700

    unshift test passing

--- day-01/array.test.js
+++ day-01/array.test.js
@@ -3,14 +3,26 @@ function push(arr, item) {
     return arr.length;
 }
 
-function unshift(arr, item) {
-   arr[]
-}
 
 test("array push", () => {
     const arr = ['a', 'b', 'c'];
     const newLength = push(arr, 'd');
     expect(arr).toEqual(['a', 'b', 'c', 'd'])
+    expect(newLength).toBe(4);
+}); 
+
+function unshift(arr, item) {
+   for(let i = arr.length; i > 0; i --){
+    arr[i] = arr[i-1];
+   }
+   arr[0] = item;
+   return arr.length;
+}
+
+test("array unshift", () => {
+    const arr = ['a', 'b', 'c'];
+    const newLength = unshift(arr, 'd');
+    expect(arr).toEqual(['d', 'a', 'b', 'c'])
     expect(newLength).toBe(4);
 }); 
 
